c9a2012a6ca42155d1bd95c85d0bd595a4b4c3b7
spitz-the-great <jspitzley85@gmail.com>
2018-08-22 17:52:28 -0500

    server up

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const dashBoardRouter = require('./routes/dashboard.router');
+const ownersRouter = require('./routes/owners.router');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.static('server/public'));
+
+app.use(bodyParser.json());
+
+// app.use('/dashboard', dashBoardRouter);
+// app.use('/owners', ownersRouter);
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
