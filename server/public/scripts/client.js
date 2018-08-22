7d10f4a81b97d47e6dcc43ec04526105f8933aee
spitz-the-great <jspitzley85@gmail.com>
2018-08-22 18:46:30 -0500

    controllers configured and working on client and index

---
+++ server/public/scripts/client.js
@@ -0,0 +1,16 @@
+var app = angular.module('PetHotelApp', ['ngRoute', 'ngMaterial']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+    console.log('Route config loaded');
+
+    $routeProvider
+        .when('/dashboard', {
+            templateUrl: 'views/dashboard.html',
+            controller: 'DashboardController as vm'
+        })
+        .when('/owners', {
+            templateUrl: 'views/owners.html',
+            controller: 'OwnersController as vm'
+        })
+        .otherwise( { template: '<h1>404</h1>' });
+}]);
