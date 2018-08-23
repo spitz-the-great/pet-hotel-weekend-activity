2e972fd0d9b55d44ebffecc6f3b03e301708149e
spitz-the-great <jspitzley85@gmail.com>
2018-08-23 12:28:03 -0500

    added image_url to pets table

--- database.sql
+++ database.sql
@@ -15,10 +15,12 @@ CREATE TABLE "pets"(
 "color" VARCHAR(20),
 "checked_in" VARCHAR(3),
 "attitude" VARCHAR(20),
-"swag_level" VARCHAR(20)
+"swag_level" VARCHAR(20),
+"image_url" VARCHAR(200)
 );
 
 
-INSERT INTO "pets" ("owner_name", "pet_name", "breed", "color", "checked_in", "attitude", "swag_level")
-VALUES ('owner1', 'tyrone biggums', 'cat', 'Hazel', 'Yes', 'high anxiety', 'non-existent');
+INSERT INTO "pets" ("owner_name", "pet_name", "breed", "color", "checked_in", "attitude", "swag_level", "image_url")
+VALUES ('owner1', 'tyrone biggums', 'cat', 'Hazel', 'Yes', 'high anxiety', 'non-existent',
+ 'https://i.pinimg.com/236x/7d/2d/57/7d2d5725cdecdc10bbc117b8f3b99c60--funny-cats-funny-animals.jpg');
 
